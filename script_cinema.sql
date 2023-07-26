bf659cba29afa826f4bda65b71f00c031514cdc3
derdeche <foued.derdeche@gmail.com>
2023-07-26 09:48:46 +0200

    modif exportation

--- script_cinema.sql
+++ script_cinema.sql
@@ -14,63 +14,148 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Listage des données de la table cinema.acteur : ~4 rows (environ)
+
+-- Listage de la structure de la base pour cinema1
+CREATE DATABASE IF NOT EXISTS `cinema1` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinema1`;
+
+-- Listage de la structure de table cinema1. acteur
+CREATE TABLE IF NOT EXISTS `acteur` (
+  `id_acteur` int NOT NULL AUTO_INCREMENT,
+  `id_personne` int NOT NULL,
+  PRIMARY KEY (`id_acteur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `acteur_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.acteur : ~4 rows (environ)
 INSERT INTO `acteur` (`id_acteur`, `id_personne`) VALUES
 	(1, 4),
 	(2, 5),
 	(3, 6),
 	(4, 7);
 
--- Listage des données de la table cinema.action : ~4 rows (environ)
+-- Listage de la structure de table cinema1. action
+CREATE TABLE IF NOT EXISTS `action` (
+  `id_film` int NOT NULL,
+  `id_genre` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_film`,`id_genre`),
+  KEY `id_genre` (`id_genre`),
+  CONSTRAINT `action_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `action_ibfk_2` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.action : ~4 rows (environ)
 INSERT INTO `action` (`id_film`, `id_genre`) VALUES
 	(1, 1),
 	(2, 2),
 	(3, 3),
 	(4, 4);
 
--- Listage des données de la table cinema.film : ~4 rows (environ)
-INSERT INTO `film` (`id_film`, `titre`, `anneeSortie`, `duree`, `synopsis`, `note`, `affiche`, `id_realisateur`) VALUES
-	(1, 'batman le defi', 1992, 126, 'Non seulement Batman doit affronter le Pingouin, monstre génétique doté d\'une intelligence à toute épreuve, qui sème la terreur mais, plus difficile encore, il doit faire face à la séduction de deux super-femmes', 4, 'batman_defi.jpg', 1),
-	(2, 'batman_forever', 1995, 135, 'Nul ne sait que Bruce Wayne, le patron d\'un vaste et puissant consortium, l\'homme le plus riche des Etats-Unis, revêt chaque nuit la combinaison et le masque de cuir de Batman pour voler au secours de ses ', 5, 'batman_forever.jpg', 2),
-	(3, 'batman et robin', 1997, 140, 'Dans cette nouvelle aventure, Batman aura bien besoin de son audacieux partenaire Robin, juché sur sa fringante moto turbo.', 3, 'robin.jpg', 2),
-	(4, 'batman begins', 2005, 145, 'Non seulement Batman doit affronter le Pingouin, monstre génétique doté d\'une intelligence à toute épreuve', 5, 'begins.jpg', 3);
+-- Listage de la structure de table cinema1. film
+CREATE TABLE IF NOT EXISTS `film` (
+  `id_film` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) NOT NULL,
+  `anneeSortie` int NOT NULL,
+  `duree` int NOT NULL,
+  `synopsis` varchar(255) NOT NULL,
+  `note` int NOT NULL,
+  `affiche` varchar(255) NOT NULL,
+  `id_realisateur` int NOT NULL,
+  PRIMARY KEY (`id_film`),
+  KEY `id_realisateur` (`id_realisateur`),
+  CONSTRAINT `film_ibfk_1` FOREIGN KEY (`id_realisateur`) REFERENCES `realisateur` (`id_realisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table cinema.genre : ~4 rows (environ)
+-- Listage des données de la table cinema1.film : ~4 rows (environ)
+INSERT INTO `film` (`id_film`, `titre`, `anneeSortie`, `duree`, `synopsis`, `note`, `affiche`, `id_realisateur`) VALUES
+	(1, 'batman le defi', 1992, 126, 'Non seulement Batman doit affronter le Pingouin, monstre génétique doté d\\\'une intelligence à toute épreuve, qui sème la terreur mais, plus difficile encore, il doit faire face à la séduction de deux super-femmes', 4, 'batman_defi.jpg', 1),
+	(2, 'batman_forever', 1995, 135, 'Nul ne sait que Bruce Wayne, le patron d\\\'un vaste et puissant consortium, l\\\'homme le plus riche des Etats-Unis, revêt chaque nuit la combinaison et le masque de cuir de Batman pour voler au secours de ses', 5, 'batman_forever.jpg', 2),
+	(3, 'batman et robin', 1997, 140, 'Dans cette nouvelle aventure, Batman aura bien besoin de son audacieux partenaire Robin, juché sur sa fringante moto turbo', 3, 'robin.jpg', 2),
+	(4, 'batman begins', 2005, 145, 'Non seulement Batman doit affronter le Pingouin, monstre génétique doté d\\\'une intelligence à toute épreuve', 5, 'begins.jpg', 3);
+
+-- Listage de la structure de table cinema1. genre
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id_genre` int NOT NULL AUTO_INCREMENT,
+  `genre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_genre`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.genre : ~4 rows (environ)
 INSERT INTO `genre` (`id_genre`, `genre`) VALUES
 	(1, 'action'),
-	(2, 'fiction'),
+	(2, 'fiction\r\n'),
 	(3, 'adventure'),
 	(4, 'comedie');
 
--- Listage des données de la table cinema.jouer : ~4 rows (environ)
+-- Listage de la structure de table cinema1. jouer
+CREATE TABLE IF NOT EXISTS `jouer` (
+  `id_film` int NOT NULL,
+  `id_acteur` int NOT NULL,
+  `id_role` int NOT NULL,
+  PRIMARY KEY (`id_film`,`id_acteur`,`id_role`),
+  KEY `id_acteur` (`id_acteur`),
+  KEY `id_role` (`id_role`),
+  CONSTRAINT `jouer_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `jouer_ibfk_2` FOREIGN KEY (`id_acteur`) REFERENCES `acteur` (`id_acteur`),
+  CONSTRAINT `jouer_ibfk_3` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.jouer : ~4 rows (environ)
 INSERT INTO `jouer` (`id_film`, `id_acteur`, `id_role`) VALUES
 	(1, 1, 1),
 	(2, 2, 1),
 	(3, 3, 1),
 	(4, 4, 1);
 
--- Listage des données de la table cinema.personne : ~7 rows (environ)
+-- Listage de la structure de table cinema1. personne
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id_personne` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `sexe` varchar(15) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  PRIMARY KEY (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.personne : ~7 rows (environ)
 INSERT INTO `personne` (`id_personne`, `nom`, `prenom`, `sexe`, `dateNaissance`) VALUES
 	(1, 'Tim', 'Burton', 'male', '1958-08-25'),
 	(2, 'Joel', 'Schumacher', 'male', '1939-08-29'),
 	(3, 'Cristopher', 'Nolan', 'male', '1970-07-30'),
-	(4, 'Michael', 'Keaton ', 'male', '1951-09-05'),
+	(4, 'Michael', 'Keaton', 'male', '1951-09-05'),
 	(5, 'Val', 'Kilmer', 'male', '1959-12-31'),
 	(6, 'George', 'Clooney', 'male', '1961-05-06'),
 	(7, 'christian', 'Bale', 'male', '1974-01-30');
 
--- Listage des données de la table cinema.realisateur : ~3 rows (environ)
+-- Listage de la structure de table cinema1. realisateur
+CREATE TABLE IF NOT EXISTS `realisateur` (
+  `id_realisateur` int NOT NULL AUTO_INCREMENT,
+  `id_personne` int NOT NULL,
+  PRIMARY KEY (`id_realisateur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `realisateur_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.realisateur : ~3 rows (environ)
 INSERT INTO `realisateur` (`id_realisateur`, `id_personne`) VALUES
 	(1, 1),
 	(2, 2),
 	(3, 3);
 
--- Listage des données de la table cinema.role : ~1 rows (environ)
-INSERT INTO `role` (`id_role`, `ROLE`) VALUES
+-- Listage de la structure de table cinema1. role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table cinema1.role : ~1 rows (environ)
+INSERT INTO `role` (`id_role`, `role`) VALUES
 	(1, 'Batman');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */ ;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
